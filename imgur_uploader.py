1c2a3b26ed2d1d14e08f32f5131a0c7d584d5828
Andrew T. Baker <andrew.tork.baker@gmail.com>
2015-04-09 23:15:58 -0400

    Switching out hard-coded API keys for environment variables

--- imgur_uploader.py
+++ imgur_uploader.py
@@ -1,16 +1,22 @@
 from imgurpython import ImgurClient
 
 import click
-
-client_id = 'f14f192893b2e09'
-client_secret = 'bca4c5cd1e0cac6eab386ab49bdca9702c04ea88'
-
-client = ImgurClient(client_id, client_secret)
+import os
 
 @click.command()
 @click.argument('gif', type=click.Path(exists=True))
 def upload_gif(gif):
     """Uploads an image file to Imgur"""
+
+    client_id = os.environ.get('IMGUR_API_ID')
+    client_secret = os.environ.get('IMGUR_API_SECRET')
+
+    if client_id is None or client_secret is None:
+        click.echo('Cannot upload - could not find IMGUR_API_ID or IMGUR_API_SECRET environment variables')
+        return
+
+    client = ImgurClient(client_id, client_secret)
+
     click.echo('Uploading file {}'.format(click.format_filename(gif)))
 
     response = client.upload_from_path(gif)
